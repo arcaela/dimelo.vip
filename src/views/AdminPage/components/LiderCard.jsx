867b2f1dda3a38be7b74592994de9798d7a7ddca
Hosni Colina <corobonhosni@gmail.com>
2021-02-09 20:08:23 -0400

    Listo el card y el modal de lideres de primer nivel

---
+++ src/views/AdminPage/components/LiderCard.jsx
@@ -0,0 +1,95 @@
+import React, { useState } from 'react'
+
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Typography from '@material-ui/core/Typography';
+import Avatar from '@material-ui/core/Avatar';
+
+import IconButton from '@material-ui/core/IconButton';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import MessageIcon from '@material-ui/icons/Message';
+import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
+
+import Delete from '../../../dist/images/trash.svg';
+
+import AvatarImg from '../../../dist/images/avatar.jpg'
+
+import { useCardLider } from './lider.styles';
+import LiderModal from './LiderModal';
+
+
+
+export default function LiderCard() {
+
+    const classes = useCardLider()
+
+    const [open, setOpen] = useState(false)
+
+
+
+    return (
+        <>        
+            <Card className={ classes.container }>
+                <div className={ classes.avatarContainer }>
+                    <Avatar className={ classes.large } src={ AvatarImg } />
+                </div>
+                <div className={ classes.cardContainer }>
+                    <div className={ classes.pRelative }>
+                        <CardHeader title="Maria Perez"/>
+                        <div className={ classes.actionsHeader } >
+                            <IconButton>
+                                <img src={ Delete } alt="eliminar"/>
+                            </IconButton>
+                        </div>
+                    </div>
+                    <CardContent className={ classes.pr }>
+                        <Typography color="textSecondary">
+                            Dirección: Los Colores - Medellín
+                        </Typography>
+                        <Typography color="textSecondary">
+                            Teléfono : +578715674877
+                        </Typography>
+                        <Typography color="textSecondary">
+                            Email: maria125d@gmail.com
+                        </Typography>
+                        <Typography color="textSecondary">
+                            Punto de votación: Escuela Bustamante
+                        </Typography>
+                        <div onClick={ ()=>{ setOpen(!open) } } className={ classes.group }>
+                            <span className={ classes.iconContainer }>
+                                <PeopleAltIcon className={ classes.icon } />
+                            </span>
+                            <span>
+                                +56
+                            </span>
+                        </div>
+                    </CardContent>
+                </div>
+                <div className={ classes.pRelative }>
+                    <CardActions>
+                        <IconButton>
+                            <InstagramIcon />
+                        </IconButton>
+                        <IconButton>
+                            <TwitterIcon />
+                        </IconButton>
+                        <IconButton>
+                            <FacebookIcon />
+                        </IconButton>
+                        <IconButton>
+                            <MessageIcon />
+                        </IconButton>
+                    </CardActions>
+                    <span className={ classes.perfil }>
+                        Pensamiento introvertido
+                    </span>
+                </div>
+            </Card>
+            <LiderModal open={ open } setOpen={ setOpen } />
+        </>
+    )
+}
