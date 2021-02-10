a88dc154ae1e2e1d7c03a7c813d862ccb2de9cbf
Hosni Colina <corobonhosni@gmail.com>
2021-02-10 19:08:58 -0400

    listo el login

--- src/views/LoginPage/LoginPage.jsx
+++ src/views/LoginPage/LoginPage.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import Login from '../../components/Login';
 import { loginStyles } from '../../components/Login/login.styles';
 import { useStylesIndex } from '../Auth/styles/index.styles';
+import { useStylesSingUp } from '../Auth/styles/signup.styles';
 
 
 export default function LoginPage() {
@@ -10,6 +11,8 @@ export default function LoginPage() {
     const classes = useStylesIndex();
 
     const loginStyle = loginStyles(); 
+
+    const body  = useStylesSingUp();
 
     return (
         <Grid container className={ classes.root }>
@@ -21,7 +24,7 @@ export default function LoginPage() {
                     </Typography>
                 </Grid>
             </Hidden>
-            <Grid item xs={12} md={6} className={` ${ classes.gridRight }  ${ loginStyle.root }`}>
+            <Grid item xs={12} md={6} className={`${ body.body } ${ classes.gridRight }  ${ loginStyle.root }`}>
                 <Login />
             </Grid>
         </Grid>
