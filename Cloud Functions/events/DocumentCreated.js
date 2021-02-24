baab3089e4df32039edfbc0d6c980200a81cc729
Alejandro Reyes <arcaela.reyes@gmail.com>
2021-02-23 22:20:32 -0400

    fix

--- Cloud Functions/events/DocumentCreated.js
+++ Cloud Functions/events/DocumentCreated.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const handlebars = require('handlebars');
-const { functions, mail } = require('../config');
+const { admin, functions, mail } = require('../config');
 
 
 
