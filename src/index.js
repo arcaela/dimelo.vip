f1a3c2703a6e72ed2a08e8b83f8d170af164690e
Alejandro Reyes <arcaela.reyes@gmail.com>
2021-03-04 12:05:10 -0400

    DatePicker Fixed

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import AuroraJS from '@arcaela/aurora';
 import './config/themes'
 import './config/routes'
 import './config/firebase'
-import { MuiPickersUtilsProvider } from '@material-ui/pickers';
-import DateFnsUtils from '@date-io/date-fns';
+
+import esLocale from "date-fns/locale/es";
+import DateFnsUtils from "@date-io/date-fns";
+import { MuiPickersUtilsProvider } from "@material-ui/pickers";
 
 ReactDOM.render(<React.StrictMode>
-    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+    <MuiPickersUtilsProvider utils={DateFnsUtils} locale={esLocale}>
         <AuroraJS />
     </MuiPickersUtilsProvider>
 </React.StrictMode>, document.getElementById('app'));
