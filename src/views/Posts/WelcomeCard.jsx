238364b97746f13b4208696db8c48f8024a5e373
Alejandro Reyes <arcaela.reyes@gmail.com>
2021-03-05 02:13:43 -0400

    Views | Posts

---
+++ src/views/Posts/WelcomeCard.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import {
+    Card,
+    makeStyles,
+    CardHeader,
+    CardMedia,
+    CardContent,
+    Avatar,
+    Typography,
+} from '@material-ui/core';
+import DimeloIcon from '~/images/logo/32x32.png';
+import DimeloWallpaper from '~/images/WELCOME.png';
+
+
+
+const useStyles = makeStyles((theme) => ({
+  root: { },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+}));
+
+export default function RecipeReviewCard() {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardHeader
+        avatar={ <Avatar src={ DimeloIcon } /> }
+        title="Dimelo VIP"
+        subheader="Administrador del sitio"
+      />
+      <CardMedia
+        className={classes.media}
+        image={ DimeloWallpaper }
+        title="Paella dish"
+      />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+            El equipo de desarrollo de <a href="/">dímelo.vip</a> te brinda una cordial bienvenida a la plataforma,
+            con gusto nos conoceremos más.
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
--- src/views/Posts/index.jsx
+++ src/views/Posts/index.jsx
@@ -1,42 +1,39 @@
 import React from "react";
 import NewsBox from "./NewsBox";
 import Layout from "~/views/layout";
+import WelcomeCard from "./WelcomeCard";
 import { api, useAuth } from "~/ServerLess";
-import { CircularProgress, Container, makeStyles } from "@material-ui/core";
+import { Button, CircularProgress, Container } from "@material-ui/core";
 
 
-const useStyles = makeStyles(()=>({
-    loading:{
-        padding:'10px 0',
-        textAlign:'center',
-    },
-}));
-const onproccess = {current:false};
+
+const $posts = [];
 export default function NewsPage(){
     const user = useAuth();
-    const classes = useStyles();
     const wall = React.useRef(null);
-    const [ posts, setPosts ] = React.useState([]);
-    const [ loading, setLoading ] = React.useState(false);
+    const [ posts, setPosts ] = React.useState($posts);
+    const [ loading, setLoading ] = React.useState(null);
     const getPosts = React.useCallback(()=>{
-        if(!loading) setLoading(onproccess.current=true);
-        api('posts/recents', {user,posts})
-            .then(docs=>setPosts(posts.concat(docs)))
-            .finally(()=>setLoading(onproccess.current=false));
-    }, [ loading, user, posts ]);
-    const AddListener = React.useCallback(()=>{
-        if(!onproccess.current && wall.current.getBoundingClientRect().bottom < 640)
-            getPosts();
-    }, [ wall, getPosts ])
-    React.useEffect(()=>{
-        if(user && !posts.length) getPosts();
-        window.addEventListener('scroll', AddListener);
-        return ()=> window.removeEventListener('scroll', AddListener);
-    });
+        if(!loading) setLoading(true);
+        api('posts/recents', { user, after:posts })
+            .then(docs=>{
+                $posts.push(...docs);
+                setPosts($posts);
+            })
+            .finally(()=>setLoading(false));
+    }, [ loading, user, posts, setPosts ]);
     return (<Layout middleware={['auth']}>
         <Container ref={wall} maxWidth="sm">
+            { !posts.length && <WelcomeCard /> }
             { posts.map((post)=><NewsBox post={post} key={post.id} />) }
-            { loading && <div className={ classes.loading } children={<CircularProgress size={25} />} /> }
+            <Button
+                fullWidth
+                color="primary"
+                variant="outlined"
+                disabled={ loading }
+                style={{marginTop:10}}
+                onClick={()=>getPosts()}
+                children={ loading?<CircularProgress size={20} />:'Cargar más' } />
         </Container>
     </Layout>);
 }
